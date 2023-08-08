bb7d4ea01902bc34a2e537acc2b7a2954fc33b2c
sakh1 <sakh1alkhimova@gmail.com>
2023-08-08 23:25:06 +0300

    исправление_1

--- src/test/java/OpenSelenideGithub.java
+++ src/test/java/OpenSelenideGithub.java
@@ -1,18 +1,34 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class OpenSelenideGithub {
     @Test
-    void searchSoftAssertionsSelenide(){
+    void searchSoftAssertionsSelenide() {
         open("https://github.com/");
         $("[placeholder='Search or jump to...']").click();
         $("[name=query-builder-test]").setValue("Selenide").pressEnter();
         $$("h3.Box-sc-g0xbh4-0").first().$("a").click();
         $("#wiki-tab").click();
-        $(byText("Soft assertions")).click();
-        $$("ol li").shouldHave(texts("TestNG listener", "JUnit4 rule", "JUnit5 extension"));
+        $(byText("Show 2 more pages…")).click();
+        $(byText("SoftAssertions")).shouldBe(visible);
+        $(byText("SoftAssertions")).click();
+        $("#user-content-3-using-junit5-extend-test-class").
+                closest("h4").
+                sibling(0).
+                shouldHave(text(
+                        "@ExtendWith({SoftAssertsExtension.class})\n" +
+                                "class Tests {\n" +
+                                "  @Test\n" +
+                                "  void test() {\n" +
+                                "    Configuration.assertionMode = SOFT;\n" +
+                                "    open(\"page.html\");\n" +
+                                "\n" +
+                                "    $(\"#first\").should(visible).click();\n" +
+                                "    $(\"#second\").should(visible).click();\n" +
+                                "  }\n" +
+                                "}"));
     }
 }
